b4a1d2725699d9bc8ff73358f77854cd633de270
Giovanna Lima <giovanna1001.lima@gmail.com>
2021-03-29 18:00:20 -0300

    v0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuCliComponent } from './menu-cli/menu-cli.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuCliComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu-cli/menu-cli.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-menu-cli',
+  templateUrl: './menu-cli.component.html',
+  styleUrls: ['./menu-cli.component.css']
+})
+export class MenuCliComponent {
+
+}
